95ed575236d68ab605898d6c7503a5837c7727a1
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-10-12 19:38:23 +0100

    aaded usermappingexception

---
+++ src/main/java/com/postitapplications/exception/exceptions/UserMappingException.java
@@ -0,0 +1,8 @@
+package com.postitapplications.exception.exceptions;
+
+public class UserMappingException extends BusinessLogicException {
+
+    public UserMappingException(String string) {
+        super(string);
+    }
+}
