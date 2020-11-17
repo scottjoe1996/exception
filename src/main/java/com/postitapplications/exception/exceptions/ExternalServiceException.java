9ed93402324c900ca6d41afa8c965f298ab2fc49
Joseph Anthony Scott <scottjoe1996@yahoo.co.uk>
2020-11-17 17:48:52 +0000

    refactored externalServiceException to carry the response as a string

--- src/main/java/com/postitapplications/exception/exceptions/ExternalServiceException.java
+++ src/main/java/com/postitapplications/exception/exceptions/ExternalServiceException.java
@@ -5,13 +5,19 @@ import org.springframework.web.client.RestClientException;
 
 public class ExternalServiceException extends RestClientException {
     private final HttpStatus statusCode;
+    private final String responseAsString;
 
-    public ExternalServiceException(HttpStatus statusCode, String errorMessage) {
+    public ExternalServiceException(HttpStatus statusCode, String errorMessage, String responseAsString) {
         super(errorMessage);
         this.statusCode = statusCode;
+        this.responseAsString = responseAsString;
     }
 
     public HttpStatus getStatusCode() {
         return statusCode;
+    }
+
+    public String getResponseAsString() {
+        return responseAsString;
     }
 }
